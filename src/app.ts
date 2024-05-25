08a54e3bd398879f13f1f5c59923b47d25c45db6
Guillaume Schurck <21091232+gschurck@users.noreply.github.com>
2024-05-25 12:15:00 +0200

    add routes initialization

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata';
 import express from 'express';
 import {NODE_ENV, PORT} from '@config';
+import {Routes} from '@interfaces/routes.interface';
 
 export class App {
   public app: express.Application;
   public env: string;
   public port: string | number;
 
-  constructor() {
+  constructor(routes: Routes[]) {
     this.app = express();
     this.env = NODE_ENV || 'development';
     this.port = PORT || 3000;
+
+    this.initializeRoutes(routes);
   }
 
   public listen() {
@@ -19,5 +22,12 @@ export class App {
 
   public getServer() {
     return this.app;
+  }
+
+
+  private initializeRoutes(routes: Routes[]) {
+    routes.forEach(route => {
+      this.app.use('/', route.router);
+    });
   }
 }
