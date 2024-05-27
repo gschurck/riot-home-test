207cf3c11a72ce7b0c9c2f07d67c45baf0138bab
Guillaume Schurck <21091232+gschurck@users.noreply.github.com>
2024-05-27 20:47:08 +0200

    load cryptography route in express app

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import {App} from '@/app';
 import {ValidateEnv} from '@utils/validateEnv';
+import {CryptographyRoute} from "@routes/cryptography.route";
 
 ValidateEnv();
 
-const app = new App([]);
+const app = new App([new CryptographyRoute()]);
 
 app.listen();
